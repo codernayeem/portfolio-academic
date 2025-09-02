7006d218558a5dc41f1cd81e4e8c4d430960467f
Md Nayeem <codernayeem898@gmail.com>
2025-09-02 18:20:42 +0000

    fixed some links & icons

--- includes/footer.php
+++ includes/footer.php
@@ -36,9 +36,10 @@
                         <li><a href="#home">Home</a></li>
                         <li><a href="#about">About</a></li>
                         <li><a href="#projects">Projects</a></li>
+                        <li><a href="#achievements">Achievements</a></li>
+                        <li><a href="#education">Education</a></li>
                         <li><a href="#contact">Contact</a></li>
-                    </ul>
-                </div>
+                    </ul>    </div>
                 <div class="footer-section">
                     <h4>Connect</h4>
                     <ul>
